6efd52cf0775ac7780f1a90f5a586aa51e16a1b4
Ubuntu <ubuntu@ip-172-31-9-150.ap-southeast-2.compute.internal>
2025-05-31 12:58:24 +0000

    user.shADDED USER FILE

---
+++ user.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo " _________USER CREATION________"
+
+echo "user_name=$1"
+echo "Password=$2"
+echo "User details $@"
+echo "__________________USER CREATION COMPLETED__________"
+
